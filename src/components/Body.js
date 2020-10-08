b24db414d0177fb85306f3d944d65059e1af922f
jayastronomic <juliansmith94@hotmail.com>
2020-10-08 10:45:19 -0500

    grammar

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,7 +8,7 @@ const Body = () => {
                       <span>Good Greenth</span> is the label, the brand and the mindset. I'm sure you and everyone knows what good means. 
                       Greenth however, is the growth of greens (herbs, vegetation). As far as I'm concerned though, greenth for me is money, weed or any prosperous situation in any regard. 
                       I've come to look at life as every move being made as a seed being planted and how it grows (or how we interact when making those moves). 
-                      Now why wouldn't I want that to be good. My favorite color is green and I love weed, so I think this all can fit into the theme quite well. 
+                      Now, why wouldn't I want that to be good!? My favorite color is green and I love weed, so I think this all can fit into the theme quite well. 
                       Each song and every bar is a seed to plant into my listeners ears to help them grow and grow with them. It's always gon be good tho 💯. 
                     </p>
                 </div>
