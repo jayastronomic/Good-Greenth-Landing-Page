fb3da4da4518215fe720e10fc9d1ab0c29122202
jayastronomic <juliansmith94@hotmail.com>
2020-10-08 22:33:01 -0500

    mobile friendly

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Player(){
     return (
 
-             <div className="lg:mx-auto lg:p-8 lg:w-3/6">
+             <div className="lg:mx-auto p-8 lg:w-3/6">
                  <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="300" style={{width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent"}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/%24tr8-drop/1534365117"></iframe>
             </div>
     )
